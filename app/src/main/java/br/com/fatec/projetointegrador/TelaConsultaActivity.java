10f4599157bcc81935c4f202962051ee169820f9
Lucas Martins <lucasmartins1096@hotmail.com>
2024-09-08 19:06:25 -0300

    terminando a tela de agendamento

--- app/src/main/java/br/com/fatec/projetointegrador/TelaConsultaActivity.java
+++ app/src/main/java/br/com/fatec/projetointegrador/TelaConsultaActivity.java
@@ -1,7 +1,6 @@
 package br.com.fatec.projetointegrador;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaAgendarActivity extends AppCompatActivity {
+public class TelaConsultaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.tela_agendar);
+        setContentView(R.layout.tela_consulta);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void consultar(View view){
-        setContentView(R.layout.tela_consulta);
     }
 }

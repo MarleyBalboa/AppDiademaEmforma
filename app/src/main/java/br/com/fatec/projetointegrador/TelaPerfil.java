70fe9b47a3f032b049de509908e576fc447d3239
Marlei dos Santos <marleymlr@hotmail.com>
2024-10-30 11:46:17 -0300

    adicionado card superior

--- app/src/main/java/br/com/fatec/projetointegrador/TelaPerfil.java
+++ app/src/main/java/br/com/fatec/projetointegrador/TelaPerfil.java
@@ -1,24 +1,30 @@
 package br.com.fatec.projetointegrador;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class TelaPerfil extends AppCompatActivity {
+
+    private Button btnPerfilVoltarInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_perfil);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnPerfilVoltarInicio = findViewById(R.id.btnPerfilVoltarInicio);
+
+        btnPerfilVoltarInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TelaPerfil.this, Home.class));
+            }
         });
     }
 }
